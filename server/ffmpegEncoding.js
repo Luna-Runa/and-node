e0da1ff86f87525bc6138d6ea98045c0432b85a6
Luna <eternality623@gmail.com>
2022-07-26 18:30:19 +0900

    final (repact)

--- server/ffmpegEncoding.js
+++ server/ffmpegEncoding.js
@@ -3,22 +3,7 @@ const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
-// ffmpeg("sample.mp4", { timeout: 432000 })
-//   .addOptions([
-//     "-profile:v baseline",
-//     "-level 3.0",
-//     "-start_number 0",
-//     "-hls_time 10",
-//     "-hls_list_size 0",
-//     "-f hls",
-//   ])
-//   .output("videos/output.m3u8")
-//   .on("end", () => {
-//     console.log("end");
-//   })
-//   .run();
-
-ffmpeg("WinterSleep1080p.mp4", { timeout: 432000 })
+ffmpeg("public/WinterSleep1080p.mp4", { timeout: 432000 })
   .addOptions([
     "-profile:v baseline", // baseline profile (level 3.0) for H264 video codec
     "-level 3.0",
@@ -32,9 +17,4 @@ ffmpeg("WinterSleep1080p.mp4", { timeout: 432000 })
   .on("end", (err, stdout, stderr) => {
     console.log("end");
   })
-  // .on("error", (err, stdout, stderr) => {
-  //   console.log(err.message);
-  //   console.log(`stdout : ${stdout}`);
-  //   console.log(`stderr : ${stderr}`);
-  // })
   .run();
--- server/server(HLS).js
+++ server/server(HLS).js
@@ -5,33 +5,26 @@ const path = require("path");
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+// ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
-ffmpeg("WinterSleep1080p.mp4", { timeout: 432000 })
-  .addOptions([
-    "-profile:v baseline", // baseline profile (level 3.0) for H264 video codec
-    "-level 3.0",
-    "-s 640x360", // 640px width, 360px height output video dimensions
-    "-start_number 0", // start the first .ts segment at index 0
-    "-hls_time 10", // 10 second segment duration
-    "-hls_list_size 0", // Maxmimum number of playlist entries (0 means all entries/infinite)
-    "-f hls", // HLS format
-  ])
-  .output("public/output.m3u8")
-  .on("end", (err, stdout, stderr) => {
-    console.log("ffmpeg end");
-  })
-  // .on("error", (err, stdout, stderr) => {
-  //   console.log(err.message);
-  //   console.log(`stdout : ${stdout}`);
-  //   console.log(`stderr : ${stderr}`);
-  // })
-  .run();
-
-// const __dirname = path.resolve();
+// ffmpeg("./public/WinterSleep1080p.mp4", { timeout: 432000 })
+//   .addOptions([
+//     "-profile:v baseline", // baseline profile (level 3.0) for H264 video codec
+//     "-level 3.0",
+//     "-s 640x360", // 640px width, 360px height output video dimensions
+//     "-start_number 0", // start the first .ts segment at index 0
+//     "-hls_time 10", // 10 second segment duration
+//     "-hls_list_size 0", // Maxmimum number of playlist entries (0 means all entries/infinite)
+//     "-f hls", // HLS format
+//   ])
+//   .output("public/output.m3u8")
+//   .on("end", (err, stdout, stderr) => {
+//     console.log("ffmpeg end");
+//   })
+//   // })
+//   .run();
 
 // // Set static directory
-// app.use(express.static(__dirname + "/public"));
 app.use(express.static("public"));
 
 // Index page
@@ -47,7 +40,7 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/test2", (req, res) => {
-  var filePath = "./WinterSleep1080p.mp4";
+  var filePath = "./public/WinterSleep1080p.mp4";
   var resolvedPath = path.resolve(filePath);
   console.log(resolvedPath);
   return res.sendFile(resolvedPath);
